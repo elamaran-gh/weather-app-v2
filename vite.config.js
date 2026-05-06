74616dd509c8d3bc463a360dcdf09953d9461b30
elamaran-gh <elamaran1928@gmail.com>
2026-05-06 21:49:49 +0530

    fix: tailwind setup corrected and build issue resolved

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
-import tailwindcss from '@tailwindcss/vite'
 import path from 'path'
 import { fileURLToPath } from 'url'
 
-// Fix for __dirname in ES Modules
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
 
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src'),
